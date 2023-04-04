3495b3499dc98545c7ed7bf6b0b53e75c43140d6
Pablo <pablochambi577@hotmail.com>
2023-04-04 19:21:03 -0300

    le quite los else if

--- PruebaEjemplo/Else_If.java
+++ PruebaEjemplo/Else_If.java
@@ -4,26 +4,17 @@ public class Else_If {
 
 	public static void main(String[] args) {
 		
-		int h = 10;
+		int h = 20;
 
 		if(h>=0 && h<=12)
-			System.out.println("Dia");
+			System.out.println("Buen dia");
 		else {
 			if(h>12 && h<=18)
-				System.out.println("Tarde");
-			else
-				System.out.println("Noche");
-		}
-		
-		
-			if(h >= 0 && h <= 12) 
-				System.out.println("Buenos dias");
-			
-			else if(h >12 && h<= 18) 
 				System.out.println("Buenas tardes");
-			
-			else 
-				System.out.println( "Buenas noches");
+			else
+				System.out.println("Buenas noches");
+		     }
+
 
 	}
 
