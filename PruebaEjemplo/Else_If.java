6580f263e43fbc59f366b5f2d450fda33d190102
Pablo <pablochambi577@hotmail.com>
2023-04-04 18:18:35 -0300

    La primera version de if else

---
+++ PruebaEjemplo/Else_If.java
@@ -0,0 +1,30 @@
+package PruebaEjemplo;
+
+public class Else_If {
+
+	public static void main(String[] args) {
+		
+		int h = 10;
+
+		if(h>=0 && h<=12)
+			System.out.println("Dia");
+		else {
+			if(h>12 && h<=18)
+				System.out.println("Tarde");
+			else
+				System.out.println("Noche");
+		}
+		
+		
+//			if(h >= 0 && h <= 12) 
+//				System.out.println("Buenos dias");
+//			
+//			else if(h >12 && h<= 18) 
+//				System.out.println("Buenas tardes");
+//			
+//			else 
+//				System.out.println( "Buenas noches");
+
+	}
+
+}
