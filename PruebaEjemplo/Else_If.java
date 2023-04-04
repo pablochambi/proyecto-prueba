27e144d5f6c4daf3b1164ed22aa2a66902b50cf5
Pablo <pablochambi577@hotmail.com>
2023-04-04 19:30:30 -0300

    version2

--- PruebaEjemplo/Else_If.java
+++ PruebaEjemplo/Else_If.java
@@ -5,16 +5,6 @@ public class Else_If {
 	public static void main(String[] args) {
 		
 		int h = 10;
-
-		if(h>=0 && h<=12)
-			System.out.println("Dia");
-		else {
-			if(h>12 && h<=18)
-				System.out.println("Tarde");
-			else
-				System.out.println("Noche");
-		}
-		
 		
 			if(h >= 0 && h <= 12) 
 				System.out.println("Buenos dias");
